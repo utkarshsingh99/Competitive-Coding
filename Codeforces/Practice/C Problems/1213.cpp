ba04baa5cb2185ade085fc23f47ba857245e8637
utkarshsingh99 <utkarshsingh369@gmail.com>
2019-09-04 22:41:10 +0530

    Practice A2OJ <1300

---
+++ Codeforces/Practice/C Problems/1213.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+//For ordered_set
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define MOD 1000000007
+#define test int t; cin>>t; while(t--)
+#define init(arr,val) memset(arr,val,sizeof(arr))
+#define loop(i,a,b) for(int i=a;i<b;i++)
+#define loopr(i,a,b) for(int i=a;i>=b;i--)
+#define loops(i,a,b,step) for(int i=a;i<b;i+=step)
+#define looprs(i,a,b,step) for(int i=a;i>=b;i-=step)
+#define ull unsigned long long int
+#define ll long long int
+#define P pair
+#define PLL pair<long long, long long>
+#define PII pair<int, int>
+#define PUU pair<unsigned long long int, unsigned long long int>
+#define L list
+#define V vector
+#define D deque
+#define ST set
+#define MS multiset
+#define M map
+#define UM unordered_map
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define MM multimap
+#define F first
+#define S second
+#define IT iterator
+#define RIT reverse_iterator
+#define FAST ios_base::sync_with_stdio(false);cin.tie();cout.tie();
+#define FILE_READ_IN freopen("input.txt","r",stdin);
+#define FILE_READ_OUT freopen("output.txt","w",stdout);
+#define all(a) a.begin(),a.end()
+using namespace std;
+// For ordered_set
+using namespace __gnu_pbds;
+template <typename T>
+using ord_set = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+const ll maxn = 1e5;
+const ll inf = 1e9;
+const double pi = acos(-1);
+
+void solve(){
+        ll n,m;
+        cin>>n>>m;
+        ll mtimesn = n/(m*10), sumtillten = 0;
+        if(mtimesn >= 1)
+            for(int i = 1; i <= 10; i++) {
+                sumtillten += (i*m)%10;
+            }
+        ll ans = sumtillten * mtimesn;
+        // cout<<sumtillten<<"\t"<<ans<<": \n";
+        ll iter = (n-mtimesn*m*10)/m;
+        for(int i = 1; i <= iter; i++) {
+            ans += (i*m)%10;
+        }
+        cout<<ans<<endl;
+}
+
+int main(){
+   int t = 0;
+   cin >> t;
+   while(t--){
+       solve();
+   }
+   return 0;
+}
